dc427aff0cbeedebba08cd179fd8c0bd3f3513dc
yusangeng <yusangeng@outlook.com>
2020-08-06 16:18:35 +0800

    chore: 使用lint-config代替本地配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,3 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "import"],
-  rules: {},
-};
+const base = require("@haier/lint-config/eslintrc");
+
+module.exports = Object.assign({}, base, {});
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,3 @@
-module.exports = {
-  printWidth: 120,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: false,
-  trailingComma: "none",
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  arrowParens: "avoid",
-  requirePragma: false,
-  proseWrap: "preserve"
-};
+const base = require("@haier/lint-config/prettierrc");
+
+module.exports = Object.assign({}, base, {});
